dc9c37aa3d87901587ee8d53070a055e73e132bc
inwoo0808 <inwoo0808@gmail.com>
2024-10-02 17:05:56 +0900

    2024-10-02

---
+++ Ex04_Jdbc_Temp/src/main/java/com/sample/spring/jdbc/MyController.java
@@ -0,0 +1,25 @@
+package com.sample.spring.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+
+@Controller
+public class MyController {
+	@Autowired
+	private MyUserDao dao;
+	
+	@RequestMapping("/")
+	public @ResponseBody  String root() {
+		return "jdbc test";
+	}
+	
+	@RequestMapping("/user")
+	public String userlistpage(Model model) {
+		System.out.println("dao()");
+		model.addAttribute("users", dao.list());
+		return "userlist";
+	}
+}
