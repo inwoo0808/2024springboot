6cf7f7f7e06061318eea72fb321523262d697bd1
inwoo0808 <inwoo0808@gmail.com>
2024-10-07 16:18:43 +0900

    2024-10-07 mybatis 실습2

--- Ex06_Mybatis_simplebbs/src/main/java/com/sample/spring/controll/MyController.java
+++ Ex06_Mybatis_simplebbs/src/main/java/com/sample/spring/controll/MyController.java
@@ -29,34 +29,34 @@ public class MyController {
 		return "list";
 	}
 	
-//	@RequestMapping("/view") // view?id=1
-//	public String view(HttpServletRequest request,Model model) {
-//		String sId = request.getParameter("id");
-//		model.addAttribute("dto", dao.viewDao(sId));
-//		return "view";
-//	}
-//	
-//	@RequestMapping("/writeForm")
-//	public String writer() {
-//		return "writeForm";
-//	}
-//	
-//	@RequestMapping("/write")
-//	public String write(HttpServletRequest request) {
-//		dao.writeDao(
-//				request.getParameter("writer"),
-//				request.getParameter("title"),
-//				request.getParameter("content")
-//				);
-//		return "redirect:list";
-//	}
-//	
-//	@RequestMapping("/delete")
-//	public String delete(HttpServletRequest request) {
-//		dao.delete(request.getParameter("id"));
-//		
-//		return "redirect:list";
-//	}
+	@RequestMapping("/view") // view?id=1
+	public String view(HttpServletRequest request,Model model) {
+		String sId = request.getParameter("id");
+		model.addAttribute("dto", dao.viewDao(sId));
+		return "view";
+	}
+	
+	@RequestMapping("/writeForm")
+	public String writer() {
+		return "writeForm";
+	}
+	
+	@RequestMapping("/write")
+	public String write(HttpServletRequest request) {
+		dao.writeDao(
+				request.getParameter("writer"),
+				request.getParameter("title"),
+				request.getParameter("content")
+				);
+		return "redirect:list";
+	}
+	
+	@RequestMapping("/delete")
+	public String delete(HttpServletRequest request) {
+		dao.delete(request.getParameter("id"));
+		
+		return "redirect:list";
+	}
 	
 	
 }
--- Ex06_Mybatis_simplebbs/src/main/java/com/sample/spring/dao/ISimpleBbsDao.java
+++ Ex06_Mybatis_simplebbs/src/main/java/com/sample/spring/dao/ISimpleBbsDao.java
@@ -3,7 +3,7 @@ package com.sample.spring.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-
+import org.apache.ibatis.annotations.Param;
 import com.sample.spring.dto.SimpleBbsDto;
 
 
@@ -13,5 +13,5 @@ public interface ISimpleBbsDao {
 	public Integer countDao();
 	public SimpleBbsDto viewDao(String id);
 	public int writeDao(String writer,String title, String content);
-	public int delete(String id);   //delete?id=1
+	public int delete(@Param("_id") String id);   //delete?id=1
 }
