be5afd0fa2ea55b50dc639fab4a59b0f6b76e1b2
inwoo0808 <inwoo0808@gmail.com>
2024-10-11 10:56:40 +0900

    2024-10-11 실습1

---
+++ Ex11_Food/src/main/java/com/sample/spring/api/FoodApi.java
@@ -0,0 +1,57 @@
+package com.sample.spring.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.spring.api.request.CreateAndEditFoodRequest;
+import com.sample.spring.model.FoodEntity;
+import com.sample.spring.service.FoodService;
+
+@RestController
+public class FoodApi {
+
+	@Autowired
+	FoodService foodService;
+	
+	@GetMapping("/foods")
+	public String getFoods() {
+		return "getFoods";
+	}
+	
+	@GetMapping("/food/{foodId}")
+	public String viewFood(
+			@PathVariable("foodId") Long foodId
+			) {
+		return "viewFood" + foodId;
+	}
+	
+	@PostMapping("/food")
+	public FoodEntity postFood(
+			@RequestBody CreateAndEditFoodRequest request
+			) {
+//		return "postFood / name: " + request.getName() + ", address : " + request.getAddress();
+		return foodService.createFood(request);
+	}
+	
+	@PutMapping("/food/{foodId}")
+	public String editFood(
+			@PathVariable("foodId") Long foodId,
+			@RequestBody CreateAndEditFoodRequest request
+			) {
+		return "editFood" + foodId + ", name: " + request.getName() + ", address : " + request.getAddress();
+	}
+	
+	@DeleteMapping("/food/{foodId}")
+	public String deleteFood(
+			@PathVariable("foodId") Long foodId
+			) {
+		return "delete" + foodId;
+	}
+	
+}
