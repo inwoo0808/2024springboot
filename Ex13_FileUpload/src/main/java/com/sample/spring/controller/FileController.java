432b07d66d8f9e11d1d3d82a44cfeb85f87c798d
inwoo0808 <inwoo0808@gmail.com>
2024-10-15 17:35:22 +0900

    2024-10-15 FileUpload 실습2

--- Ex13_FileUpload/src/main/java/com/sample/spring/controller/FileController.java
+++ Ex13_FileUpload/src/main/java/com/sample/spring/controller/FileController.java
@@ -6,7 +6,10 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,4 +40,22 @@ public class FileController {
 				).collect(Collectors.toList());
 		return ResponseEntity.status(HttpStatus.OK).body(uploadResult);
 	}
+	
+	@GetMapping("/file/{id}")
+	public ResponseEntity<byte[]> downImage(
+			@PathVariable("id") Long id
+			) throws IOException{
+			byte[] downloadImage = fileDataService.downloadImageFileSystem(id);
+
+			if(downloadImage != null) {
+				return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/png")).body(downloadImage);
+			}else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
+			
+	}
+	
+	
+	
+	
 }
